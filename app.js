f627dc054753eedd79aaedecaf5acec7fb207855
hdzhu5 <hdzhu5@hdzhu5-ubuntu.(none)>
2013-04-07 13:59:51 -0400

    Heroku working and headers institutionalized.

--- app.js
+++ app.js
@@ -27,8 +27,11 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-app.get('/', routes.index);
+app.get('/', user.index);
 app.get('/users', user.list);
+app.get('/research', user.research);
+app.get('/lab_members', user.lab_members);
+app.get('/news', user.news);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
--- routes/user.js
+++ routes/user.js
@@ -6,3 +6,19 @@
 exports.list = function(req, res){
   res.send("respond with a resource");
 };
+
+exports.index = function(req, res){
+  res.render('index', { title: 'Huang Lab | Olin College' });
+};
+
+exports.research = function(req, res){
+  res.render('research', { title: 'Research | Huang Lab' });
+};
+
+exports.lab_members = function(req, res){
+  res.render('lab_members', { title: 'Lab Members | Huang Lab' });
+};
+
+exports.news = function(req, res){
+  res.render('news', { title: 'News | Huang Lab' });
+};

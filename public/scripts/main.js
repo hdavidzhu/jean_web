df0f14a16e6097bd9666c514a634591acafea6c1
hdzhu5 <huanzhen.zhu@students.olin.edu>
2013-06-13 13:47:14 -0400

    Even more updates

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
+
+	$('.tab').hover( function(){
+        $(this).toggleClass('green');
+    });
+
+    $('.youtube_button').hover( function(){
+        $(this).toggleClass('green');
+    });
+
+	// Stops Youtube embedding from bugging out.
 	$('iframe').each(function(){
           var url = $(this).attr("src");
           var char = "?";
@@ -7,4 +17,5 @@ $(document).ready(function() {
            }
           $(this).attr("src",url+char+"wmode=transparent");
     });
+
 });

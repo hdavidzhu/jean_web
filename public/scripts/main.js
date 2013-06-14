e9249f35caa004063072a7c741dee41e2c5d734d
hdzhu5 <huanzhen.zhu@students.olin.edu>
2013-06-14 08:59:20 -0400

    Trying to get slideshow working

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,21 +1,26 @@
-$(document).ready(function() {
+$(document).ready( function() {
 
+  // Makes tabs turn green when hovered over.
 	$('.tab').hover( function(){
-        $(this).toggleClass('green');
-    });
+    $(this).toggleClass('green');
+  });
 
-    $('.youtube_button').hover( function(){
-        $(this).toggleClass('green');
-    });
+  // For the slideshow.
+  $('#slideshow-div').rsfSlideshow();
+
+  // Makes youtube button turn green when hovered.
+  $('.youtube_button').hover( function(){
+    $(this).toggleClass('green');
+  });
 
 	// Stops Youtube embedding from bugging out.
-	$('iframe').each(function(){
-          var url = $(this).attr("src");
-          var char = "?";
-          if(url.indexOf("?") != -1){
-                  var char = "&";
-           }
-          $(this).attr("src",url+char+"wmode=transparent");
-    });
+  $('iframe').each( function(){
+    var url = $(this).attr("src");
+    var char = "?";
+    if(url.indexOf("?") != -1){
+      var char = "&";
+    }
+    $(this).attr("src",url+char+"wmode=transparent");
+  });
 
 });
